0dd407c0e7dd8af4c5ff10cbfe0fd249c1063c82
Troy Leach <troyleach29@gmail.com>
2018-10-18 22:06:23 -0400

    WIP checking if the request changes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,11 @@
 class AnswersController < ApplicationController
   def get_answer
-
-    render body: 'OK'
+    list_of_questions_and_keys = {
+      "Ping" => "OK",
+      "Name" => "Troy Wade Leach",
+      "Status" => "Yes"
+    }
+    result = list_of_questions_and_keys[params["q"]]
+    render body: result
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,9 +2,47 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   describe 'GET /answer' do
-    it 'it returns an answer' do
-      get :get_answer
-      expect(response).to have_http_status(:success)
+    context 'making sure 200' do
+      it 'it returns an answer' do
+        get :get_answer
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'returns "ok"' do
+      it 'returns the string OK' do
+        params = {"q"=>"Ping",
+                  "d"=>"Please return OK so that I know your service works."}
+        get :get_answer, params: params
+        expect(response.body).to eq('OK')
+      end
+    end
+
+    context 'returns "Link to the source code"' do
+      xit 'returns the string OK' do
+        params = {"q"=>"Source",
+                  "d"=>"Please provide a URL where we can download the source code of your resume submission web service."}
+        get :get_answer, params: params
+        expect(response.body).to eq('OK')
+      end
+    end
+
+    context 'returns Full Name' do
+      it 'returns the string OK' do
+        params = {"q"=>"Name",
+                  "d"=>"What is your full name?"}
+        get :get_answer, params: params
+        expect(response.body).to eq('Troy Wade Leach')
+      end
+    end
+
+    context 'returns Status' do
+      it 'returns the string OK' do
+        params = {"q"=>"Status",
+                  "d"=>"Can you provide proof of eligibility to work in the US?"}
+        get :get_answer, params: params
+        expect(response.body).to eq('Yes')
+      end
     end
   end
 end
