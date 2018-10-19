94f402b885f9762ca6a09a53c40e6096b7491f31
Troy Leach <troyleach29@gmail.com>
2018-10-19 14:36:35 -0400

    WID added email getting more puzzle results

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,8 +3,13 @@ class AnswersController < ApplicationController
     list_of_questions_and_keys = {
       "Ping" => "OK",
       "Name" => "Troy Wade Leach",
+      "Email" => "troyleach@outlook.com",
       "Status" => "Yes"
     }
+    #if params["q"] == 'Puzzle'
+     #return result = 'ABCD\nA=>><\nB<=><\nC<<=<\nD>>>=\n'
+    #end
+
     result = list_of_questions_and_keys[params["q"]]
     render body: result
   end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe 'puzzle' do
+    it 'it returns an answer' do
+      params = {"q"=>"Puzzle",
+                "d"=>"Please solve this puzzle:\n ABCD\nA=>--\nB<---\nC-<=-\nD>---\n" }
+      get :get_answer, params: params
+      expect(response.body).to eq('ABCD\nA=>><\nB<=><\nC<<=<\nD>>>=\n')
+    end
+  end
+
   describe 'GET /answer' do
     context 'making sure 200' do
       it 'it returns an answer' do
@@ -42,6 +51,15 @@ RSpec.describe AnswersController, type: :controller do
                   "d"=>"Can you provide proof of eligibility to work in the US?"}
         get :get_answer, params: params
         expect(response.body).to eq('Yes')
+      end
+    end
+
+    context 'returns Email address' do
+      it 'returns the string email' do
+        params = {"q"=>"Email Address", "d"=>"What is your email address?"}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('troyleach@outlook.com')
       end
     end
   end
