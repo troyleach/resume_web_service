41be869ac200435d51041d8683ed351e3e1360fe
Troy Leach <troyleach29@gmail.com>
2018-10-20 12:28:44 -0400

    finished the answer to the questions I know

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,19 +2,30 @@ class AnswersController < ApplicationController
   include SolvePuzzle
 
   def get_answer
-    list_of_keys_and_answers = {
-      "Ping" => "OK",
-      "Name" => "Troy Wade Leach",
-      "Email Address" => "troyleach@outlook.com",
-      "Status" => "Yes"
-    }
-
     if params['q'] == 'Puzzle'
       result = solve_puzzle(params['d'])
     else
-      result = list_of_keys_and_answers[params["q"]]
+      result = troy_leach_resume[params["q"]]
     end
 
     render body: result
+  end
+
+  private
+
+  def troy_leach_resume
+    {
+      "Ping" => "OK",
+      "Name" => "Troy Wade Leach",
+      "Email Address" => "troyleach@outlook.com",
+      "Phone" => "720-552-0720",
+      "Position" => "Senior Full Stack Engineer",
+      "Years" => "4 Years of experience",
+      "Referrer" => "https://angel.co/",
+      "Degree" => "RoR Web Development, Actualize, HTML and Web Development, Colorado Free University",
+      "Resume" => "https://www.google.com",
+      "Source" => "https://www.google.com",
+      "Status" => "Yes"
+    }
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   describe 'puzzle' do
     xit 'it returns an answer' do
-      #TODO test fails, algorithm
+      #TODO test fails, algorithm not correct
       params = {"q"=>"Puzzle",
                 "d"=>"Please solve this puzzle:\n ABCD\nA=>--\nB<---\nC-<=-\nD>---\n" }
       get :get_answer, params: params
-      expect(response.body).to eq("ABCD\nA=>><\nB<=><\nC<<=<\nD>>>=\n")
+      expect(response.body).to eq(solved_puzzle)
     end
   end
 
   describe 'GET /answer' do
-    context 'making sure 200' do
+    context 'expect 200' do
       it 'it returns an answer' do
         get :get_answer
         expect(response).to have_http_status(:success)
@@ -20,7 +20,7 @@ RSpec.describe AnswersController, type: :controller do
     end
 
     context 'returns "ok"' do
-      it 'returns the string OK' do
+      it 'positive result' do
         params = {"q"=>"Ping",
                   "d"=>"Please return OK so that I know your service works."}
         get :get_answer, params: params
@@ -28,17 +28,8 @@ RSpec.describe AnswersController, type: :controller do
       end
     end
 
-    context 'returns "Link to the source code"' do
-      xit 'returns the string OK' do
-        params = {"q"=>"Source",
-                  "d"=>"Please provide a URL where we can download the source code of your resume submission web service."}
-        get :get_answer, params: params
-        expect(response.body).to eq('OK')
-      end
-    end
-
     context 'returns Full Name' do
-      it 'returns the string OK' do
+      it 'positive result' do
         params = {"q"=>"Name",
                   "d"=>"What is your full name?"}
         get :get_answer, params: params
@@ -46,8 +37,97 @@ RSpec.describe AnswersController, type: :controller do
       end
     end
 
+    context 'returns Email address' do
+      it 'positive result' do
+        params = {"q"=>"Email Address", "d"=>"What is your email address?"}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('troyleach@outlook.com')
+      end
+    end
+
+    context 'returns phone number' do
+      it 'positive result' do
+
+        params = {"q"=>"Phone", "d"=>"Please provide a phone number we can use to reach you."}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('720-552-0720')
+      end
+    end
+
+    context 'Position applying for' do
+      it 'positive result' do
+
+        params = {"q"=>"Position", "d"=>"Which position at Balihoo are you applying for?"}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('Senior Full Stack Engineer')
+      end
+    end
+
+    context 'Years of experience' do
+      it 'positive result' do
+
+        params = {"q"=>"Years", "d"=>"How many years of software development experience do you have?"}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('4 Years of experience')
+      end
+    end
+
+    context 'How did you hear about this position' do
+      it 'positive result' do
+
+        params = {"q"=>"Referrer", "d"=>"How did you hear about this position?"}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('https://angel.co/')
+      end
+    end
+
+    context 'Relevant degrees(s)' do
+      it 'positive result' do
+
+        params = {"q"=>"Degree", "d"=>"Please list your relevant university degree(s)."}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('RoR Web Development, Actualize, HTML and Web Development, Colorado Free University')
+      end
+    end
+
+    context 'Relevant degrees(s)' do
+      it 'positive result' do
+
+        params = {"q"=>"Degree", "d"=>"Please list your relevant university degree(s)."}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('RoR Web Development, Actualize, HTML and Web Development, Colorado Free University')
+      end
+    end
+
+    context 'Resume and Cover letter' do
+      it 'positive result' do
+
+        params = {"q"=>"Resume", "d"=>"Please provide a URL where we can download your resume and cover letter."}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('https://www.google.com')
+      end
+    end
+
+    context 'URL for resume web service' do
+      it 'positive result' do
+
+        params = {"q"=>"Source", "d"=>"Please provide a URL where we can download the source code of your resume submission web service."}
+
+        get :get_answer, params: params
+        expect(response.body).to eq('https://www.google.com')
+      end
+    end
+
     context 'returns Status' do
-      it 'returns the string OK' do
+      it 'positive result' do
         params = {"q"=>"Status",
                   "d"=>"Can you provide proof of eligibility to work in the US?"}
         get :get_answer, params: params
@@ -55,18 +135,10 @@ RSpec.describe AnswersController, type: :controller do
       end
     end
 
-    context 'returns Email address' do
-      it 'returns the string email' do
-        params = {"q"=>"Email Address", "d"=>"What is your email address?"}
-
-        get :get_answer, params: params
-        expect(response.body).to eq('troyleach@outlook.com')
-      end
-    end
   end
 end
 
-def puzzle_solved
+def solved_puzzle
   #{"q"=>"Puzzle", "d"=>"Please solve this puzzle:\n ABCD\nA=>--\nB<---\nC-<=-\nD>---\n"}
   "ABCD\nA=>><\nB<=><\nC<<=<\nD>>>=\n"
 end
